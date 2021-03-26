33c47562534ac885cf6ed1a7ffce3bbc0f0f1797
7700250090 <phelipe.matos-ext@viavarejo.com.br>
2021-03-26 12:06:42 -0300

    Implementação do 'MatButtonModule' para o componente 'CardComponent'

--- src/app/card/card.module.ts
+++ src/app/card/card.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { CardComponent } from './card.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule
   ],
   exports: [CardComponent],
   declarations: [CardComponent]
